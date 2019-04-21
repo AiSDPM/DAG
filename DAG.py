59c1d85e5586d5d87b0062f88fb4ecaee9da0b20
Michal <pm.aisd.pp@gmail.com>
2019-04-21 18:11:08 +0100

    test

--- DAG.py
+++ DAG.py
@@ -13,13 +13,47 @@ class GraphM :
             for j in range(i+1, n):
                 if j == i+1 or  (random.random() + 2/n) < (g/100):
                     self.matrix[i][j] = 1
+        for i in range(n):
+            x = random.randint(0,n-1)
+            y = random.randint(0,n-1)
+            if x != y:
+                self.matrix[x], self.matrix[y] =  self.matrix[y],  self.matrix[x]
+                if self.matrix[x][x] ==1:
+                    self.matrix[x][x] = 0
+                    self.matrix[x][y] = 1
+                elif self.matrix[y][y] ==1:
+                    self.matrix[y][y] = 0
+                    self.matrix[y][x] = 1
 
-    def print (self):
+
+
+    def __str__(self):
+        out = ""
         for i in range (len(self.matrix)):
             pom = ""
             for j in range(len(self.matrix)):
                 pom = pom + str(self.matrix[i][j]) + " "
-            print (pom)
+            out = out + "\n" + pom
+        return out
+
+    def _TS(self, IN, Sort, step, v):
+        step +=1
+        IN[v] = step
+        for  i in range(len(self.matrix)):
+            if self.matrix[v][i] != 0 and IN[i] == -1:
+                self._TS(IN, Sort, step, i)
+        Sort.insert(0,v)
+
+
+    def TS(self,start):
+        IN = [-1]*len(self.matrix)
+        step = 0
+        Sort = []
+        self._TS(IN, Sort, step,start)
+        return Sort
+
+
+
 
 
 class Edge:
@@ -40,16 +74,37 @@ class GraphL:
                 if  val != 0:
                    self.list[i].append(j)
 
-    def print (self):
+    def __str__(self):
         l = len(self.list)
+        out = ""
         for i in range (l):
             pom = str(i)+ ":  "
             li = len(self.list[i])
             for j in range(li):
                 pom = pom + str(self.list[i][j]) + " "
-            print (pom)
+            out = out + "\n" + pom
+        return out
+
+    def _TS(self,IN, Sort, step, v):
+        step +=1
+        IN[v] = step
+        for  i in range(len(self.list[v])):
+            if IN[self.list[v][i]] == -1:
+               self._TS(IN, Sort, step, self.list[v][i])
+        Sort.insert(0,v)
+
+
+    def TS(self,start):
+        IN = [-1]*len(self.list)
+        step = 0
+        Sort = []
+        self._TS(IN, Sort, step, start)
+        return Sort
 
 HelloWorld = GraphM(10,60)
-HelloWorld.print()
+print(HelloWorld)
 HelloWorldL = GraphL(HelloWorld)
-HelloWorldL.print()
+print(HelloWorldL)
+
+print(HelloWorld.TS(0))
+print(HelloWorldL.TS(0))
--- aPrim.py
+++ aPrim.py
@@ -15,18 +15,66 @@ class GraphM :
                     self.matrix[j][i] = self.matrix[i][j]
 
 
-    def print (self):
+    def __str__(self):
+        out = ""
         for i in range (len(self.matrix)):
             pom = ""
             for j in range(len(self.matrix)):
                 pom = pom + str(self.matrix[i][j]) + " "
-            print (pom)
+            out = out + "\n" + pom
+        return out
+
+    def prim (self, start):
+        VMST = [start]
+        EMST = []
+        edges = []
+        for i in range(len(self.matrix)):
+            if self.matrix[start][i] != 0:
+                edges.append(Edge(start, i, self.matrix[start][i]))
+        while len(EMST) < len(self.matrix)-1:
+
+            E = Edge()
+            count = 0
+            while count < len(edges):
+                if edges[count].x in VMST and edges[count].y in VMST:
+                    edges.remove(edges[count])
+                else:
+                    if edges[count].val < E.val:
+                        E = edges[count]
+                    count +=1
+
+
+            if not edges:
+                break
+
+            edges.remove(E)
+            VMST.append(E.y)
+            EMST.append(E)
+
+            for i in range(len(self.matrix)):
+                if self.matrix[E.y][i] != 0:
+                    edges.append(Edge(E.y, i, self.matrix[E.y][i]))
+        return EMST
+
+
 
 class Edge:
-    def __init__(self,u,v, val):
+    def __init__(self,x = -1 ,y =-1 , val = 9999):
         self.x = x
         self.y = y
         self.val = val
+
+    def __str__(self):
+        out =  "[("+str(self.x) + "," + str(self.y)+");"  + str(self.val) + "]"
+        return out
+
+class dEdge:
+    def __init__(self ,y =-1 , val = 9999):
+        self.y = y
+        self.val = val
+    def __str__(self):
+        out =  "[(" + str(self.y)+");"  + str(self.val) + "]"
+        return out
 
 
 class GraphL:
@@ -38,18 +86,59 @@ class GraphL:
             for j in range(l):
                 val = matrix.matrix[i][j]
                 if  val != 0:
-                   self.list[i].append((j, val))
+                   self.list[i].append(dEdge(j, val))
 
-    def print (self):
+    def __str__(self):
         l = len(self.list)
+        out = ""
         for i in range (l):
             pom = str(i)+ ":  "
             li = len(self.list[i])
             for j in range(li):
                 pom = pom + str(self.list[i][j]) + " "
-            print (pom)
+            out = out + "\n" + pom
+        return out
+
+    def prim(self, start):
+        VMST = [start]
+        EMST = []
+        edges = []
+        for i in range(len(self.list[start])):
+                edges.append(Edge(start, self.list[start][i].y, self.list[start][i].val))
+        while len(EMST) < len(self.list) - 1:
+            E = Edge()
+            count = 0
+            while count < len(edges):
+                if edges[count].x in VMST and edges[count].y in VMST:
+                    edges.remove(edges[count])
+                else:
+                    if edges[count].val < E.val:
+                        E = edges[count]
+                    count += 1
+
+            if not edges:
+                break
+            edges.remove(E)
+            VMST.append(E.y)
+            EMST.append(E)
+            for i in range(len(self.list[E.y])):
+                    edges.append(Edge(E.y, self.list[E.y][i].y, self.list[E.y][i].val))
+        return EMST
+
 
 HelloWorld = GraphM(10,30)
-HelloWorld.print()
+print(HelloWorld)
 HelloWorldL = GraphL(HelloWorld)
-HelloWorldL.print()
+print(HelloWorldL)
+
+
+print("Macierz")
+out = HelloWorld.prim(0)
+for i in out:
+    print(i)
+
+print("Lista")
+
+out = HelloWorldL.prim(0)
+for i in out:
+    print(i)
